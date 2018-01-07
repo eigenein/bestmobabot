414a04099d974cd704c014a42a259b4d345efca1
Pavel Perestoronin <eigenein@gmail.com>
2018-01-07 23:08:02 +0100

    Remove star import

--- bestmobabot/constants.py
+++ bestmobabot/constants.py
@@ -1,8 +1,12 @@
-from bestmobabot.types import *
+from datetime import timedelta
+
+from bestmobabot import types
 
 
-EXPEDITION_COLLECT_REWARD = ExpeditionStatus(2)
-EXPEDITION_FINISHED = ExpeditionStatus(3)
+EXPEDITION_COLLECT_REWARD = types.ExpeditionStatus(2)
+EXPEDITION_FINISHED = types.ExpeditionStatus(3)
 
-QUEST_IN_PROGRESS = QuestState(1)
-QUEST_COLLECT_REWARD = QuestState(2)
+QUEST_IN_PROGRESS = types.QuestState(1)
+QUEST_COLLECT_REWARD = types.QuestState(2)
+
+DAY = timedelta(days=1)
