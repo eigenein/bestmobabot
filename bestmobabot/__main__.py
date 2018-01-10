17ae888e4bd3977b16cb269355e9ec3fc10d604b
Pavel Perestoronin <eigenein@gmail.com>
2018-01-10 19:26:45 +0100

    Fix battle log open mode

--- bestmobabot/__main__.py
+++ bestmobabot/__main__.py
@@ -14,7 +14,7 @@ from bestmobabot.logger import logger
 @click.command()
 @click.option('-s', '--remixsid', help='VK.com remixsid cookie.', envvar='BESTMOBABOT_REMIXSID', required=True)
 @click.option('--no-experience', help='Do not farm experience.', envvar='BESTMOBABOT_NO_EXPERIENCE', is_flag=True)
-@click.option('--battle-log', help='Log battles results into JSON Lines file.', envvar='BESTMOBABOT_BATTLE_LOG', type=click.File('wt'))
+@click.option('--battle-log', help='Log battles results into JSON Lines file.', envvar='BESTMOBABOT_BATTLE_LOG', type=click.File('at'))
 @click.option('-v', '--verbose', help='Increase verbosity.', is_flag=True)
 @click.option('-l', '--log-file', help='Log file.', envvar='BESTMOBABOT_LOGFILE', type=click.File('wt'), default=click.get_text_stream('stderr'))
 def main(remixsid: str, no_experience: bool, battle_log: Optional[TextIO], verbose: bool, log_file: TextIO):
