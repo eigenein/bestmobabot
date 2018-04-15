177b816e19c01984eb6b13a58dc44026e02c6c03
Pavel Perestoronin <eigenein@gmail.com>
2018-04-15 14:04:03 +0200

    Starting database wrapper

--- bestmobabot/database.py
+++ bestmobabot/database.py
@@ -3,7 +3,11 @@ Database wrapper.
 """
 
 import contextlib
+import json
 import sqlite3
+from typing import Callable, Optional, TypeVar
+
+T = TypeVar('T')
 
 
 class Database(contextlib.AbstractContextManager):
@@ -14,4 +18,12 @@ class Database(contextlib.AbstractContextManager):
         self.connection.__exit__(exc_type, exc_value, traceback)
 
     def ensure_table(self, table_name: str):
-        ...
+        with self.connection.cursor() as cursor:  # type: sqlite3.Cursor
+            cursor.execute(f'CREATE TABLE IF NOT EXISTS "{table_name}" (key TEXT PRIMARY KEY, value TEXT)')
+
+    def get(self, table_name: str, key: str, loads: Callable[[str], T] = json.loads) -> Optional[T]:
+        self.ensure_table(table_name)
+        with self.connection.cursor() as cursor:  # type: sqlite3.Cursor
+            cursor.execute(f'SELECT value FROM "{table_name}" WHERE key = %s', (key,))
+            row = cursor.fetchone()
+            return loads(row[0]) if row else None
