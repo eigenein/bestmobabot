290956e3cae6b1e3631bcbd796f5ddf70e47b436
Pavel Perestoronin <eigenein@gmail.com>
2018-05-20 14:56:07 +0200

    Fix circular import issue

--- bestmobabot/logger.py
+++ bestmobabot/logger.py
@@ -5,28 +5,28 @@ Logger initialisation.
 import logging
 from typing import Iterable
 
-from bestmobabot.responses import *
+import bestmobabot.responses
 
 
 logger = logging.getLogger('bestmobabot')
 
 
-def log_heroes(emoji: str, message: str, heroes: Iterable[Hero]):
+def log_heroes(emoji: str, message: str, heroes: Iterable['bestmobabot.responses.Hero']):
     logger.info(f'{emoji} {message}')
-    for hero in sorted(heroes, reverse=True, key=Hero.order):
+    for hero in sorted(heroes, reverse=True, key=bestmobabot.responses.Hero.order):
         logger.info(f'{emoji} {hero}')
 
 
-def log_reward(reward: Reward):
+def log_reward(reward: 'bestmobabot.responses.Reward'):
     reward.log(logger)
 
 
-def log_rewards(rewards: Iterable[Reward]):
+def log_rewards(rewards: Iterable['bestmobabot.responses.Reward']):
     for reward in rewards:
         log_reward(reward)
 
 
-def log_arena_result(result: ArenaResult):
+def log_arena_result(result: 'bestmobabot.responses.ArenaResult'):
     logger.info('👍 You won!' if result.win else '👎 You lose.')
     for i, battle in enumerate(result.battles, start=1):
         logger.info(f'👊 Battle #{i}: {"⭐" * battle.stars if battle.win else "lose."}')
