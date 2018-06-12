22d11351810d6fda7c753273029246958d02e899
Pavel Perestoronin <eigenein@gmail.com>
2018-06-12 11:40:55 +0200

    Fix analytics

--- bestmobabot/analytics.py
+++ bestmobabot/analytics.py
@@ -12,7 +12,7 @@ session = requests.Session()
 
 def send_event(*, category: str, action: str, label: Optional[str] = None, user_id: str):
     try:
-        with session.post(URL, data={'v': 1, 'tid': TID, 't': 'event', 'ec': category, 'ea': action, 'el': label, 'uid': user_id}) as response:
+        with session.post(URL, data={'v': 1, 'tid': TID, 't': 'event', 'ec': category, 'ea': action, 'el': label, 'cid': user_id}) as response:
             response.raise_for_status()
     except Exception as ex:
         logger.warning('😱 Failed to send the event.', exc_info=ex)
@@ -20,7 +20,7 @@ def send_event(*, category: str, action: str, label: Optional[str] = None, user_
 
 def send_exception(*, description: str, user_id: str):
     try:
-        with session.post(URL, data={'v': 1, 'tid': TID, 't': 'exception', 'exd': description, 'exf': '1', 'uid': user_id}) as response:
+        with session.post(URL, data={'v': 1, 'tid': TID, 't': 'exception', 'exd': description, 'exf': '1', 'cid': user_id}) as response:
             response.raise_for_status()
     except Exception as ex:
         logger.warning('😱 Failed to send the exception.', exc_info=ex)
