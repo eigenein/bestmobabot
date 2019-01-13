bd9444326f14ca558ab88a33dce201473dac4784
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2019-01-13 17:53:14 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='bestmobabot',
-    version='2.4b6',
+    version='2.4b7',
     author='Pavel Perestoronin',
     author_email='eigenein@gmail.com',
     description='Hero Wars game bot 🏆',
