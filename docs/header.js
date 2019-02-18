3a6bf3996221163e90a9fcee62d31d2c37f13786
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2019-02-18 22:23:19 +0100

    Update libs, successfully simulate battle

--- docs/header.js
+++ docs/header.js
@@ -1,8 +1,6 @@
 // Run:
 // cat header.js heroes.js footer.js | sed 's/h.AS3 = L;/h.AS3 = L; window.h = h;/g' | node
 
-const { performance } = require('perf_hooks');
-
 var window = {
     document: {
         createElement: function() {
@@ -18,7 +16,7 @@ var window = {
     navigator: {
         userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36',
     },
-    performance: performance,
+    performance: require('perf_hooks').performance,
 };
 
 var fs = require('fs');
