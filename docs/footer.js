abb30ae358fd8e1df71b604a1aaa8cadc73d38fb
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2019-02-18 21:11:57 +0100

    First success

--- docs/footer.js
+++ docs/footer.js
@@ -1,202 +1,6 @@
 (function (h) {
-    var response = {
-        "userId": "833061",
-        "typeId": -1000553,
-        "attackers": {
-            "4021": {
-                "id": 4021,
-                "xp": 0,
-                "level": 1,
-                "star": 1,
-                "skills": {
-                    "4021": 1
-                },
-                "power": 157,
-                "artifacts": [
-                    {
-                        "level": 1,
-                        "star": 0
-                    },
-                    {
-                        "level": 1,
-                        "star": 0
-                    },
-                    {
-                        "level": 1,
-                        "star": 0
-                    }
-                ],
-                "scale": 0.8,
-                "anticrit": 1,
-                "antidodge": 1,
-                "hp": 17045,
-                "physicalAttack": 1041,
-                "element": "earth",
-                "elementSpiritLevel": 1,
-                "elementSpiritStar": 0,
-                "state": {
-                    "hp": 17045,
-                    "energy": 600,
-                    "isDead": false
-                }
-            },
-            "4022": {
-                "id": 4022,
-                "xp": 3060,
-                "level": 18,
-                "star": 1,
-                "skills": {
-                    "4023": 18
-                },
-                "power": 926,
-                "artifacts": [
-                    {
-                        "level": 1,
-                        "star": 0
-                    },
-                    {
-                        "level": 1,
-                        "star": 0
-                    },
-                    {
-                        "level": 1,
-                        "star": 0
-                    }
-                ],
-                "scale": 0.8,
-                "anticrit": 1,
-                "antidodge": 1,
-                "hp": 109001.04,
-                "physicalAttack": 5450.05,
-                "element": "earth",
-                "elementSpiritLevel": 1,
-                "elementSpiritStar": 0,
-                "state": {
-                    "hp": 109001,
-                    "energy": 800,
-                    "isDead": false
-                }
-            },
-            "4020": {
-                "id": 4020,
-                "xp": 32210,
-                "level": 55,
-                "star": 4,
-                "skills": {
-                    "4019": 55
-                },
-                "power": 14063,
-                "artifacts": [
-                    {
-                        "level": 43,
-                        "star": 2
-                    },
-                    {
-                        "level": 4,
-                        "star": 2
-                    },
-                    {
-                        "level": 10,
-                        "star": 1
-                    }
-                ],
-                "scale": 0.8,
-                "anticrit": 1,
-                "antidodge": 1,
-                "hp": 1554744.26,
-                "physicalAttack": 51903.58,
-                "elementAttack": 33891.6,
-                "elementArmor": 939.6,
-                "element": "earth",
-                "elementSpiritLevel": 1,
-                "elementSpiritStar": 0,
-                "state": {
-                    "hp": 1512035,
-                    "energy": 1000,
-                    "isDead": false
-                }
-            }
-        },
-        "defenders": [
-            {
-                "4002": {
-                    "id": 4002,
-                    "xp": 0,
-                    "level": 26,
-                    "star": 1,
-                    "skills": {
-                        "4005": 26
-                    },
-                    "power": 1499,
-                    "artifacts": [
-                        {
-                            "level": 1,
-                            "star": 0
-                        },
-                        {
-                            "level": 1,
-                            "star": 0
-                        },
-                        {
-                            "level": 1,
-                            "star": 0
-                        }
-                    ],
-                    "scale": 0.8,
-                    "anticrit": 1,
-                    "antidodge": 1,
-                    "hp": 147041.51,
-                    "physicalAttack": 11028.09,
-                    "element": "water",
-                    "elementSpiritLevel": 1,
-                    "elementSpiritStar": 0
-                },
-                "4000": {
-                    "id": 4000,
-                    "xp": 0,
-                    "level": 26,
-                    "star": 1,
-                    "skills": {
-                        "4001": 26
-                    },
-                    "power": 1504,
-                    "artifacts": [
-                        {
-                            "level": 1,
-                            "star": 0
-                        },
-                        {
-                            "level": 1,
-                            "star": 0
-                        },
-                        {
-                            "level": 1,
-                            "star": 0
-                        }
-                    ],
-                    "scale": 0.8,
-                    "anticrit": 1,
-                    "antidodge": 1,
-                    "hp": 161744.96,
-                    "physicalAttack": 9992.07,
-                    "element": "water",
-                    "elementSpiritLevel": 1,
-                    "elementSpiritStar": 0
-                }
-            }
-        ],
-        "effects": [],
-        "reward": {
-            "dungeonActivity": 5,
-            "fragmentTitan": {
-                "4012": 1
-            }
-        },
-        "startTime": 1549574686,
-        "seed": 2747195978,
-        "type": "dungeon_titan",
-        "rewardMultiplier": 1
-    };
+    var response = {"userId":"833061","typeId":"-220009","attackers":{"39":{"id":39,"xp":3557555,"level":129,"color":10,"slots":[0,0,0,0],"skills":{"195":129,"196":129,"197":129,"198":129},"power":47884,"star":6,"runes":[490,1420,13970,43750,490],"skins":{"52":3,"82":38},"currentSkin":82,"titanGiftLevel":21,"titanCoinsSpent":{"consumable":{"24":29350}},"artifacts":[{"level":61,"star":5},{"level":61,"star":3},{"level":63,"star":3}],"scale":1,"agility":1790,"hp":60865,"intelligence":1661,"physicalAttack":2035,"strength":5014.1999999999998,"armor":9955.7999999999993,"magicResist":4779.8000000000002,"skin":82},"25":{"id":25,"xp":3557591,"level":129,"color":12,"slots":{"3":0,"4":0,"1":0,"2":0},"skills":{"125":129,"126":129,"127":129,"128":129},"power":53327,"star":6,"runes":[9850,1050,6920,1000,1100],"skins":{"25":47},"currentSkin":25,"titanGiftLevel":24,"titanCoinsSpent":{"consumable":{"24":39550}},"artifacts":[{"level":93,"star":4},{"level":92,"star":3},{"level":93,"star":3}],"scale":1,"agility":6511,"hp":10815,"intelligence":1827,"physicalAttack":4993.1999999999998,"strength":2611,"armor":100,"armorPenetration":7221.6000000000004,"magicResist":466,"physicalCritChance":653,"skin":25},"35":{"id":35,"xp":3557303,"level":129,"color":10,"slots":{"3":0,"1":0,"4":0,"0":0,"2":0},"skills":{"175":129,"176":129,"177":129,"178":129},"power":38720,"star":6,"runes":[0,0,0,0,0],"skins":{"79":21},"currentSkin":79,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":56,"star":4},{"level":58,"star":3},{"level":69,"star":2}],"scale":1,"agility":1665,"hp":20315,"intelligence":2476,"physicalAttack":50,"strength":3894.3000000000002,"armor":2022.2,"magicPower":1796,"magicResist":3619.1999999999998,"skin":79},"13":{"id":13,"xp":3557571,"level":129,"color":10,"slots":{"3":0,"1":0,"2":0,"0":0,"4":0},"skills":{"62":129,"63":129,"64":129,"65":129},"power":42958,"star":6,"runes":[50,0,570,0,50],"skins":{"13":5,"38":36},"currentSkin":38,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":82,"star":4},{"level":78,"star":2},{"level":79,"star":2}],"scale":1,"agility":1889,"hp":15420,"intelligence":4049.3000000000002,"physicalAttack":50,"strength":2023,"armor":700,"magicPenetration":3613.6999999999998,"magicPower":8871.3999999999996,"magicResist":947,"skin":38},"29":{"id":29,"xp":3557580,"level":129,"color":10,"slots":{"1":0,"4":0,"0":0,"2":0,"3":0},"skills":{"145":129,"146":129,"147":129,"148":129},"power":35571,"star":6,"runes":[0,0,50,0,0],"skins":[],"currentSkin":0,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":49,"star":3},{"level":44,"star":2},{"level":38,"star":1}],"scale":1,"agility":1869,"hp":28865.099999999999,"intelligence":3464,"physicalAttack":106,"strength":2003,"armor":781,"magicPower":3610.8000000000002,"magicResist":747,"skin":0}},"defenders":[{"4":{"id":"4","xp":0,"level":"39","color":4,"slots":[],"skills":{"255":"39","256":"39","257":"39"},"power":5402,"star":"3","runes":[0,0,0,0,0],"skins":[],"currentSkin":0,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":1,"star":0},{"level":1,"star":0},{"level":1,"star":0}],"scale":1,"agility":295,"hp":2740,"intelligence":300,"physicalAttack":105,"strength":372,"armor":175,"magicPower":25,"magicResist":175,"skin":0,"state":{"hp":17620,"energy":0,"isDead":false}},"1":{"id":"1","xp":0,"level":"39","color":4,"slots":[],"skills":{"2":"39","3":"39","4":"39"},"power":4668,"star":"2","runes":[0,0,0,0,0],"skins":[],"currentSkin":0,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":1,"star":0},{"level":1,"star":0},{"level":1,"star":0}],"scale":1,"agility":183,"hp":2855,"intelligence":239,"physicalAttack":133,"strength":282,"armor":100,"magicPower":150,"magicResist":50,"skin":0,"state":{"hp":14135,"energy":0,"isDead":false}},"9":{"id":"9","xp":0,"level":"39","color":4,"slots":[],"skills":{"42":"39","43":"39","44":"39"},"power":4088,"star":"1","runes":[0,0,0,0,0],"skins":[],"currentSkin":0,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":1,"star":0},{"level":1,"star":0},{"level":1,"star":0}],"scale":1,"agility":147,"hp":500,"intelligence":251,"physicalAttack":50,"strength":147,"armor":125,"magicPower":125,"magicResist":125,"skin":0,"state":{"hp":6380,"energy":0,"isDead":false}},"5":{"id":"5","xp":0,"level":"39","color":4,"slots":[],"skills":{"101":"39","102":"39","24":"39"},"power":5405,"star":"3","runes":[0,0,0,0,0],"skins":[],"currentSkin":0,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":1,"star":0},{"level":1,"star":0},{"level":1,"star":0}],"scale":1,"agility":257,"hp":1200,"intelligence":474,"physicalAttack":50,"strength":262,"magicPower":300,"magicResist":175,"skin":0,"state":{"hp":11680,"energy":0,"isDead":false}},"19":{"id":"19","xp":0,"level":"39","color":4,"slots":[],"skills":{"92":"39","93":"39","94":"39"},"power":5398,"star":"3","runes":[0,0,0,0,0],"skins":[],"currentSkin":0,"titanGiftLevel":0,"titanCoinsSpent":null,"artifacts":[{"level":1,"star":0},{"level":1,"star":0},{"level":1,"star":0}],"scale":1,"agility":382,"hp":2285,"intelligence":311,"physicalAttack":246,"strength":306,"magicResist":25,"skin":0,"state":{"hp":14525,"energy":0,"isDead":false}}}],"effects":[],"reward":{"dungeonActivity":1,"consumable":{"20":25}},"startTime":1550520469,"seed":3392282911,"type":"clan_dungeon","rewardMultiplier":1};
+    // {"result":{"win":true,"stars":3},"progress":[{"v":151,"b":0,"seed":-902684385,"attackers":{"input":[],"heroes":{"13":{"hp":96340,"energy":945,"isDead":false},"25":{"hp":115255,"energy":1000,"isDead":false},"29":{"hp":108985,"energy":400,"isDead":false},"35":{"hp":176087,"energy":209,"isDead":false},"39":{"hp":261101,"energy":100,"isDead":false}}},"defenders":{"input":[],"heroes":{}}}],"isRaid":true}
 
     console.debug('Get classes.');
     var Bytes = h['haxe.io.Bytes']; // `ib`
@@ -204,9 +8,11 @@
     var BattlePresets = h['game.battle.controller.thread.BattlePresets']; // `sj`
     var DataStorage = h['game.data.storage.DataStorage'];  // `v`
     var AssetStorage = h['game.assets.storage.AssetStorage']; // `r`
+    var BattleAssetStorage = h['game.assets.storage.BattleAssetStorage']; // `sh`
+    var BattleLog = h['battle.BattleLog']; // `Ra`
 
     console.debug('Load skills.sc.');
-    AssetStorage.battle = new h['game.assets.storage.BattleAssetStorage'];
+    AssetStorage.battle = new BattleAssetStorage();
     AssetStorage.battle.loadEncodedCode(new Bytes(Array.from(fs.readFileSync('skills.sc'))));
     
     h.JsPakoCompression.init();
@@ -218,16 +24,18 @@
     console.debug('Init battle instant play.');
     var presets = new BattlePresets(false, false, true, DataStorage.battleConfig.get_titan(), false);
     var play = new BattleInstantPlay(response, presets);
+    play.battleData.attackers.initialize(AssetStorage.battle.skillFactory.bind(AssetStorage.battle));
+    play.battleData.defenders.initialize(AssetStorage.battle.skillFactory.bind(AssetStorage.battle));
 
     console.debug('Execute battle.');
     play.executeBattle();
 
     console.debug('Create result.');
+    BattleLog.m.bytes.getEncodedString = function() { return this.bytes }; // This freezes.
     play.createResult();
     var result = play.get_result();
 
     console.debug('Get result.');
-    console.debug(result.progressData);
-    console.debug(JSON.stringify(result.get_result()));
-    console.debug(JSON.stringify(result.get_progress()));
+    console.debug(result.get_result());
+    console.debug(result.get_progress());
 })(window.h)
